4412a9f692e3788ed4dc2dcc62e7285108a5e2bf
shushikata <shu.42111227@gmail.com>
2020-08-01 12:12:27 +0000

    [Finish]bk2-before_debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_user!
+  
+	protect_from_forgery with: :exception
+
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
@@ -12,6 +14,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email] )
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,22 @@
 class BooksController < ApplicationController
+                
+  before_action :authenticate_user!
+  before_action :ensure_correct_book, only: [:edit, :update]
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.new
+    @user = @book.user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -22,8 +29,6 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-
-
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -33,7 +38,7 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
     @book.destoy
     redirect_to books_path
@@ -42,7 +47,14 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_book
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
